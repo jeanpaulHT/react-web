8013fb87ee027ff008f3fbfedd5621f0a3039a05
jeanpaulHT <jean.huby@utec.edu.pe>
2023-04-27 18:17:26 -0500

    A

---
+++ mui-test/src/App.tsx
@@ -0,0 +1,13 @@
+import Face from './face.tsx'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Face/>
+      <hr style={{width: "100vw" }} />
+    </div>
+  );
+}
+
+export default App;
